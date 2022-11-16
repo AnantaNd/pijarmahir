68fd2229bd40a976bf0a5a95a5278e0217f0e772
AnantaNd <ananta.nov2@gmail.com>
2022-11-17 06:57:42 +0700

    CardSignUp: add logic validator email

--- components/Card/CardSignUp/CardSignUp.js
+++ components/Card/CardSignUp/CardSignUp.js
@@ -1,12 +1,29 @@
+import { useState } from 'react'
+import validator from 'validator'
 import Button from '../../Button/Button'
 import Input from '../../Input/Input'
 import styles from './CardSignUp.module.css'
+
 export default function CardSignUp() {
+  const [emailError, setEmailError] = useState('')
+
+  const validate =(e)=>{
+    var email = e.target.value
+    console.log(email)
+    if(validator.isEmail(email)){
+      setEmailError(`Kami akan mengirim kode verifikasi ke ${email}`)
+    }else{
+      setEmailError('Format email yang dimasukkan tidak memiliki “@”')
+    }
+  }
+
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>daftar</h1>
       <p className={styles.subtitle}>bangun karirmu bersama pijar mahir</p>
-      <Input label="Email" name="email" type="email" placeholder="example@g,ail.com" />
+      <Input onChangeInput={(e)=>validate(e)} label="Email" name="email" type="email" placeholder="example@gmail.com" />
+      <p className={styles.desc}>{emailError}</p>
       <Button buttonType="primary">Lanjutkan</Button>
       <p className={styles.label_login}>Sudah memiliki akun? <button className={styles.btn_login}>masuk</button></p>
       <p className={styles.policy}>Dengan mendaftar, Anda menyetujui Ketentuan <span className={styles.syarat}>Penggunaan</span> dan <span className={styles.syarat}>Kebijakan Privasi </span>kami.</p>
--- components/Input/Input.js
+++ components/Input/Input.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Styles from './Input.module.css';
 
-function Input({ label, type, name, placeholder, helper }) {
+function Input({ label, type, name, placeholder, helper, onChangeInput}) {
   return (
     <div className={Styles.container_input}>
       <label className={Styles.label_input} htmlFor={name}>{label}</label>
-      <input className={Styles.input} name={name} type={type} placeholder={placeholder} />
+      <input className={Styles.input} name={name} type={type} placeholder={placeholder} onChange={onChangeInput}/>
       <small className={Styles.helper}>{helper}</small>
     </div>
   )
