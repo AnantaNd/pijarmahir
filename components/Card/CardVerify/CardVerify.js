1ced96da95bea0018f8de4cc990e1c0b137a39b3
AnantaNd <ananta.nov2@gmail.com>
2022-11-17 07:43:03 +0700

    verify: add page verify and fix styling component CardVerify

--- components/Card/CardVerify/CardVerify.js
+++ components/Card/CardVerify/CardVerify.js
@@ -7,13 +7,15 @@ export default function CardVerify(){
       <div className={styles.content}>
         <h1 className={styles.title}>masukan kode verifikasi</h1>
         <p className={styles.subtitle}>kode verifikasi telah dikirim ke email</p>
-        <div className={styles.input_form}>
-          <input className={input_verify}/>
-          <input className={input_verify}/>
-          <input className={input_verify}/>
-          <input className={input_verify}/>
+        <div className={styles.input_container}>
+          <input className={styles.input_verify}/>
+          <input className={styles.input_verify}/>
+          <input className={styles.input_verify}/>
+          <input className={styles.input_verify}/>
         </div>
         <Button buttonType="primary">Verifikasi</Button>
+        <p className={styles.subtitle}>Tidak menerima kode? <span>Kirim Ulang</span></p>
+
       </div>
     </div>
   )
