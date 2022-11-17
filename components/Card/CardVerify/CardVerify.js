8c69d533ba3e011939cee416c1d453c9dcede62d
AnantaNd <ananta.nov2@gmail.com>
2022-11-17 07:08:11 +0700

    CardVerify: add component verify

---
+++ components/Card/CardVerify/CardVerify.js
@@ -0,0 +1,20 @@
+import Button from '../../Button/Button'
+import styles from './CardVerify.module.css'
+
+export default function CardVerify(){
+  return(
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>masukan kode verifikasi</h1>
+        <p className={styles.subtitle}>kode verifikasi telah dikirim ke email</p>
+        <div className={styles.input_form}>
+          <input className={input_verify}/>
+          <input className={input_verify}/>
+          <input className={input_verify}/>
+          <input className={input_verify}/>
+        </div>
+        <Button buttonType="primary">Verifikasi</Button>
+      </div>
+    </div>
+  )
+}
