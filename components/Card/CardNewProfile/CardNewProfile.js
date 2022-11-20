d5f22d677a3742e72f82818aba688f478a457ab4
AnantaNd <ananta.nov2@gmail.com>
2022-11-20 07:59:42 +0700

    newprofile: Add page new profile

--- components/Card/CardNewProfile/CardNewProfile.js
+++ components/Card/CardNewProfile/CardNewProfile.js
@@ -7,9 +7,9 @@ export default function CardNewProfile(){
     <div className={styles.container}>
       <h1 className={styles.title}>data profil</h1>
       <p className={styles.subtitle}>lengkapi profil sesuai identitas kamu</p>
-      <Input onChangeInput={(e) => validate(e)} label="nama-legkap" name="name" type="text" />
-      <p>pastikan sesuai dengan ktp untuk persyaratan settifikat</p>
-      <Input onChangeInput={(e) => validate(e)} label="no-telepon" name="tel" type="tel" pattern="[0-9]{4}-[0-9]{4}-[0-9]{4}"/>
+      <Input label="Nama Legkap" name="name" type="text" />
+      <p className={styles.desc}>pastikan sesuai dengan ktp untuk persyaratan settifikat</p>
+      <Input label="No Telepon" name="tel" type="tel" pattern="[0-9]{4}-[0-9]{4}-[0-9]{4}"/>
       <Button buttonType='primary'>Selesai</Button>
     </div>
   )
