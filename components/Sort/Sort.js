5cf1da86b4e89191e1e1960f0c66c1ef1cd8cf7e
AnantaNd <ananta.nov2@gmail.com>
2022-12-06 23:31:44 +0700

    Sort: fix stylin sorting

--- components/Sort/Sort.js
+++ components/Sort/Sort.js
@@ -1,11 +1,11 @@
-import { FaFilter } from 'react-icons/fa'
-import styles from './Sort.module.css'
+import { BiSliderAlt } from "react-icons/bi";
+import styles from './Sort.module.css';
 
 export default function Sort({onSorting}){
   return (
     <div className={styles.container}>
       <div className={styles.filter}>
-        <FaFilter className={styles.icon_filter} size={18}></FaFilter>
+        <BiSliderAlt className={styles.icon_filter} size={19}></BiSliderAlt>
         <h1 className={styles.title}>Filter</h1>
       </div>
       <select onChange={onSorting} className={styles.sort}>
