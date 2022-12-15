f71078fa37e95087cbae15a31008cbf0f06380aa
AnantaNd <ananta.nov2@gmail.com>
2022-12-15 23:45:28 +0700

    Landing Page: fecthing api

--- components/Category/Category.js
+++ components/Category/Category.js
@@ -13,7 +13,7 @@ function Category({ title, data }) {
           data?.map((d, idx) => {
             return (
               <div key={idx} className={Styles.card}>
-                <Image alt={'img'} className={Styles.card_img} src={d.img} width={255} height={135} style={{ borderRadius: "4px" }} />
+                <Image alt={'img'} className={Styles.card_img} src={'/mahirprakerja.jpg'} width={255} height={135} style={{ borderRadius: "4px" }} />
                 <p className={title && Styles.text_center}>
                   {d.categoryname}
                 </p>
